1a636bf6adb74716ee2bd8c91605a563ed51f277
awyehle <35705055+awyehle@users.noreply.github.com>
2018-01-30 12:35:09 -0600

    Add files via upload

---
+++ BowlingTest1.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit38ClassRunner;
+
+public class BowlingTest1 {
+	
+	
+	@Test
+	public void testOneThrow() {
+		
+	}
+	
+	@Test
+	public void testTwoThrows() {
+		
+	}
+	
+	@Test
+	public void testThreeThrows() {
+		
+	}
+	
+	@Test
+	public void testThrowOn11thFrame() {
+		
+	}
+
+	
+
+}
---
+++ BowlingTest2.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BowlingTest2 {
+	TestSuite testSuite1 = new TestSuite();
+	TestSuite testSuite2 = new TestSuite();
+	TestSuite testSuite3 = new TestSuite();
+	TestSuite testSuite4 = new TestSuite();
+	TestSuite testSuite5 = new TestSuite();
+	
+	@Test
+	public void testSpareCountsNextFrameScore() {
+		
+	}
+	
+	@Test
+	public void testStrikeMovesToNextFrame() {
+		
+	}
+	
+	@Test
+	public void testStrikeCountsNextFrameScores() {
+		
+	}
+	
+	@Test
+	public void testSpareOnLastFrame() {
+		
+	}
+	
+	@Test
+	public void testStrikeOnLastFrames() {
+		
+	}
+
+}
