4080fb5534fc7c443645f09b79f664540a5febae
The Batman <The Batman@10.22.160.86>
2018-02-22 16:10:11 -0600

    gdgdf

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,4 +1,17 @@
 
 public class Sensor {
 
+	private Chronotimer _connection;
+	
+	public void setConnection(Chronotimer connection)
+	{
+		this._connection=connection;
+	}
+	
+	public void trip()
+	{
+		if(_connection==null)
+			return;
+		_connection.
+	}
 }
