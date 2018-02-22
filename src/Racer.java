4fcf480e731008aa5f7a01c4842b1e0f4ff5a823
andrew <andrew@10.22.164.104>
2018-02-22 16:24:16 -0600

    Created Racer class logic

--- src/Racer.java
+++ src/Racer.java
@@ -1,4 +1,20 @@
-
+/**
+ * 
+ * @author Andrew Yehle
+ *Racer object has a bib number to identify racers.
+ *
+ */
 public class Racer {
 
+	int bib = -1;
+	
+	public Racer(int bib){
+		this.bib = bib;
+	}
+	
+	public int getBib(){
+		
+		return this.bib;
+	}
+	
 }
