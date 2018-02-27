f7637654ec1bdfdc06a7edebc76c1ce9c89cee67
almig <almig@AndrewPC.mshome.net>
2018-02-27 08:30:47 -0600

    Created initial test cases

--- src/ChronoTester.java
+++ src/ChronoTester.java
@@ -1,7 +1,85 @@
+import org.junit.Test;
 
 public class ChronoTester {
 		
 		static Chronotimer newChronotimer = new Chronotimer();
+		
+		@Test
+		public void TestPower()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestReset()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestCancel()
+		{
+			
+			
+		}
+		
+		@Test
+		public void StartEvent()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestNewRun()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestToggleSensor()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestEnterNewRacer()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestTripSensorChannel1()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestTripSensorChannel2()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestPrint()
+		{
+			
+			
+		}
+		
+		@Test
+		public void TestEndRun()
+		{
+			
+			
+		}
 		
 		
 		
