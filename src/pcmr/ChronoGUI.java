84c2fd704fa42b1f2bb104dc64bd443a3a59b154
almig <almig@AndrewPC.mshome.net>
2018-04-15 12:00:01 -0500

    GUI added channel numbers

--- src/pcmr/ChronoGUI.java
+++ src/pcmr/ChronoGUI.java
@@ -24,6 +24,14 @@ public class ChronoGUI {
 	private JTextField txtChronotimer;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
 
 	/**
 	 * Launch the application.
@@ -256,5 +264,101 @@ public class ChronoGUI {
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 12));
 		label_1.setBounds(132, 87, 90, 14);
 		frame.getContentPane().add(label_1);
+		
+		textField_2 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_2.setText("1");
+		textField_2.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_2.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_2.setColumns(10);
+		textField_2.setBackground(SystemColor.menu);
+		textField_2.setBounds(238, 32, 27, 14);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_3.setText("3");
+		textField_3.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_3.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_3.setColumns(10);
+		textField_3.setBackground(SystemColor.menu);
+		textField_3.setBounds(275, 32, 27, 14);
+		frame.getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_4.setText("5");
+		textField_4.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_4.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_4.setColumns(10);
+		textField_4.setBackground(SystemColor.menu);
+		textField_4.setBounds(312, 32, 27, 14);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_5.setText("7");
+		textField_5.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_5.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_5.setColumns(10);
+		textField_5.setBackground(SystemColor.menu);
+		textField_5.setBounds(349, 32, 27, 14);
+		frame.getContentPane().add(textField_5);
+		
+		textField_6 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_6.setText("4");
+		textField_6.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_6.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_6.setColumns(10);
+		textField_6.setBackground(SystemColor.menu);
+		textField_6.setBounds(275, 108, 27, 14);
+		frame.getContentPane().add(textField_6);
+		
+		textField_7 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_7.setText("6");
+		textField_7.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_7.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_7.setColumns(10);
+		textField_7.setBackground(SystemColor.menu);
+		textField_7.setBounds(312, 108, 27, 14);
+		frame.getContentPane().add(textField_7);
+		
+		textField_8 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_8.setText("8");
+		textField_8.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_8.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_8.setColumns(10);
+		textField_8.setBackground(SystemColor.menu);
+		textField_8.setBounds(349, 108, 27, 14);
+		frame.getContentPane().add(textField_8);
+		
+		textField_9 = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		textField_9.setText("2");
+		textField_9.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_9.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		textField_9.setColumns(10);
+		textField_9.setBackground(SystemColor.menu);
+		textField_9.setBounds(238, 108, 27, 14);
+		frame.getContentPane().add(textField_9);
 	}
 }
